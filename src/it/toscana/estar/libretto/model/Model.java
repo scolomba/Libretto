d31f318bd5a7c3681c876d72bdaa4e57381f82a8
s.colomba <s.colomba@MSC-ESTAR>
2018-03-13 15:12:59 +0100

    Costruzione del Model

--- src/it/toscana/estar/libretto/model/Model.java
+++ src/it/toscana/estar/libretto/model/Model.java
@@ -1,5 +1,47 @@
 package it.toscana.estar.libretto.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {
+	private List<Esame> esami;
+	
+	public Model() {
+		this.esami = new ArrayList<Esame>();
+		
+	}
+	/**
+	 * Aggiunge un nuovo esame all'elenco degli esami presenti
+	 * verificando che non ci sia già
+	 * @param e
+	 * @return true se l'hai inserito, false se esisteva già e quindi non inserisci
+	 */
+	public boolean addEsame(Esame e) {
+		if(!esami.contains(e)) {
+		    esami.add(e);
+		    return true;
+		}else {
+			return false;
+		}
+			
+	}
+	
+	/**
+	 * Ricerca se esiste un esame con il codice specificato
+	 * Se esiste lo restituisce
+	 * altrimenti restituisce null
+	 * @param codice
+	 * @return l'esame trovato, oppure null se non trovato
+	 */
+	public Esame trovaEsame(String codice) {
+		int pos = this.esami.indexOf(new Esame(codice, null,null));
+		if(pos== -1) {
+			return null;
+	    }else {
+	    	return esami.get(pos);
+	    }
+		
+	}
+	
 
 }
---
+++ src/it/toscana/estar/libretto/model/TestModel.java
@@ -0,0 +1,19 @@
+package it.toscana.estar.libretto.model;
+
+public class TestModel {
+
+	public static void main(String[] args) {
+		Model m = new Model();
+		
+		System.out.println(m.addEsame(new Esame("03FYZ","Tecniche di programmazione", "Fulvio Corno")));
+		System.out.println(m.addEsame(new Esame("01QZP","Ambient Intelligence", "Fulvio Corno")));
+		System.out.println(m.addEsame(new Esame("03FYZ","Tecniche di programmazione", "Fulvio Corno")));
+		
+		System.out.println(m.trovaEsame("03FYZ"));
+		System.out.println(m.trovaEsame("01QZP"));
+		System.out.println(m.trovaEsame("99QQQ"));
+		
+
+	}
+
+}
